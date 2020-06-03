1f86ecdd1880e1a9264b7efdc8e502701b2eb27c
kelly <kellyp.alves87@gmail.com>
2020-06-03 20:58:10 -0300

    funções de sign in e newaccount

--- public/pages/newAccount/data.js
+++ public/pages/newAccount/data.js
@@ -1,23 +1,23 @@
 export function handleSignUp() {
-  let email = document.querySelector("#user-email").value;
-  let password = document.querySelector("#user-pass").value;
-  let loginButton = document.querySelector("#login-button").value;
+  const email = document.querySelector('#user-email').value;
+  const password = document.querySelector('#user-pass').value;
+  const loginButton = document.querySelector('#login-button').value;
   if (email.length <= 5) {
-    alert("Por favor insira um endereço de e-mail válido.");
+    alert('Por favor insira um endereço de e-mail válido.');
     return;
   }
   if (password.length <= 6) {
-    alert("Por favor insira uma senha.");
+    alert('Por favor insira uma senha.');
     return;
   }
   firebase
     .auth()
-    .createUserWithEmailAndPassword(email, password)
+    .createUserWithEmailAndPassword(email, password, loginButton)
     .catch(function (error) {
-      let errorCode = error.code;
-      let errorMessage = error.message;
-      if (errorCode === "auth/senha muito fraca") {
-        alert("Senha muito fraca.");
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      if (errorCode === 'auth/senha muito fraca') {
+        alert('Senha muito fraca.');
       } else {
         alert(errorMessage);
       }
