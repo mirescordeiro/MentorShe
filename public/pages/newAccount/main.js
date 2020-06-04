d46cca0d89f572d929000675b33682280f9a5309
Tamires Cordeiro <tamirescordeiro@Mac-mini-de-Tamires.local>
2020-06-04 12:35:09 -0300

    html5 inputs + refatoração css + favicon

--- public/pages/newAccount/main.js
+++ public/pages/newAccount/main.js
@@ -3,26 +3,17 @@
 export const newAccount = () => {
   const container = document.createElement('div');
 
-  container.innerHTML = `<section id="new-account" class="flex-left form">
+  container.innerHTML = `<form id="new-account" class="flex column form">
     <h2>Criar uma nova conta</h2>
     <label for="email">Email</label>
-      <input id="account-user" type="text" placeholder="email@host.com.br">
+    <input id="account-user" type="email" placeholder="email@host.com.br" required>
   
-      <label for="password">Senha</label>
-      <input id="account-pass" type="password" placeholder="mínimo 6 caracteres">
-      
-      <label for="pass">Confirmação de senha</label>
-      <input id="confirm-pass" type="password" placeholder="mínimo 6 caracteres">
-      
-      <label for="name">Nome</label>
-      <input id="user-name" type="text" placeholder="Maria">
+    <label for="password">Senha</label>
+    <input id="account-pass" type="password" placeholder="mínimo 6 caracteres" requires>
   
-      <label for="last-name">Sobrenome</label>
-      <input id="last-name" type="text" placeholder="Carneiro">
-      
-      <button id="create-count" type="submit">CADASTRE-SE</button>
-      <p class="footer">Já tem uma conta? <a href="#login">Acesse agora</a></p>
-      </section>`;
+    <button id="create-count" type="submit">CADASTRE-SE</button>
+    <p class="footer">Já tem uma conta? <a href="#login">Acesse agora</a></p>
+  </form>`;
 
   return container;
 };
@@ -32,3 +23,7 @@ export const newAccount = () => {
 // const signinButton = document.querySelector('#create-count');
 
 // signinButton.addEventListener('click', handleSignUp(email, password));
+
+// <label for="pass">Confirmação de senha</label> <input id="confirm-pass" type="password" placeholder="mínimo 6 caracteres">
+// <label for="last-name">Sobrenome</label> <input id="last-name" type="text" placeholder="Carneiro">
+// <label for="name">Usuário</label> <input id="user-name" type="text" placeholder="Nome Sobrenome" required>
