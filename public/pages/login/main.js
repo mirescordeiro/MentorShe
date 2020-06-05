c74671f833ea0fc3332f5e94cd0ee237c11640ef
Tamires Cordeiro <mirescordeiro@gmail.com>
2020-06-05 20:00:17 -0300

    imagem register + css

--- public/pages/login/main.js
+++ public/pages/login/main.js
@@ -6,7 +6,7 @@ export const login = () => {
         <img src="./img/login_mobile.svg" alt="Ilustração de dois navegadores abertos, no primeiro há uma seta grande que clica no segundo, no segundo há uma ovelha com uma mensagem de emoticon de coração" class="mobile">
         <img src="./img/login_desktop.svg" alt="Ilustração de três navegadores abertos, no primeiro há um coração, no segundo uma seta de correto, no terceiro uma emoticon sorrindo. No centro entre eles temos uma ovelha" class="desktop">
       </figure>
-      <div class="flex center column login">
+      <form class="flex center column login">
         <h1>mentor<strong>she</strong></h1>
         <p class="welcome mobile">&#60;seja bem-vinda!&#62;</p>
         <p class="welcome desktop">&#60;mentoria para todas!&#62;</p>
@@ -15,12 +15,8 @@ export const login = () => {
         <input id="user-pass" type="password" placeholder="Senha" required>
         <span id="pass-alert" class="alert"></span>
         <button id="login-button" type="submit">ENTRAR</button>
-        <label>
-      <input class="remember-me" type="checkbox" checked="checked" name="remember"> Lembre-me
-      </label>      
-      <span class="psw">Esqueceu a <a href="#">senha?</a></span>
         <p class="footer">Não possui uma conta? <a href="#newAccount">Crie uma conta</a></p>
-      </div>
+      </form>
     </section>
   `;
 
--- public/pages/newAccount/main.js
+++ public/pages/newAccount/main.js
@@ -1,21 +1,26 @@
 export const newAccount = () => {
   const container = document.createElement('div');
 
-  container.innerHTML = `<form id="new-account" class="flex column form">
-    <h2>Criar uma nova conta</h2>
-    <label for="email">Email</label>
-    <input id="account-user" type="email" placeholder="email@host.com.br" required>
-    <span id="email-alert" class="alert"></span>
+  container.innerHTML = `<section id="new-account" class="flex center row-desk form">
+    <figure>
+      <img src="./img/register.svg" alt="Ilustração de monitor e dois usuários" class="desktop">
+    </figure>
+    <div class="flex center column form">
+      <h2>Criar uma nova conta</h2>
+      <form class="flex column register">
+        <label for="email">Email</label>
+        <input id="account-user" type="email" placeholder="email@host.com.br" required>
+        <span id="email-alert" class="alert"></span>
 
-    <label for="password">Senha</label>
-    <input id="account-pass" type="password" placeholder="mínimo 6 caracteres" requires>
-    <span id="pass-alert" class="alert"></span>
+        <label for="password">Senha</label>
+        <input id="account-pass" type="password" placeholder="mínimo 6 caracteres" required>
+        <span id="pass-alert" class="alert"></span>
 
-    <span id="validation"></span>
-  
-    <button id="create-count" type="submit">CADASTRE-SE</button>
-    <p class="footer">Já tem uma conta? <a href="#login">Acesse agora</a></p>
-  </form>`;
+      </form>
+      <button id="create-count" type="submit">CADASTRE-SE</button>
+      <p class="footer">Já tem uma conta? <a href="#login">Acesse agora</a></p>
+    </div>
+  </section>`;
 
   return container;
 };
