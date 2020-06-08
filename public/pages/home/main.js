801c872784ab4ffeb883a23dd9635cea11c73eb3
larissamiyaji <natsumi-23@hotmail.com>
2020-06-08 14:30:24 -0300

    Início da função onAuthStateChanged

--- public/pages/home/main.js
+++ public/pages/home/main.js
@@ -1,5 +1,5 @@
 // Aqui serão criados os eventos de Manipulação de DOM e templates
-import { greeting } from './data.js';
+// import { greeting } from './data.js';
 
 export const home = () => {
   const container = document.createElement('div');
