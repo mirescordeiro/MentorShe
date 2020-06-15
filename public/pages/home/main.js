b9ad053fb31afbd5c8b9561951e2752c6f6214b5
larissamiyaji <natsumi-23@hotmail.com>
2020-06-15 10:15:10 -0300

    Identação

--- public/pages/home/main.js
+++ public/pages/home/main.js
@@ -1,12 +1,12 @@
 // Aqui serão criados os eventos de Manipulação de DOM e templates
-import { newPost, loadPosts, deletePost, likePost, logout } from "./data.js";
-//import { loadPosts } from './data.js';
-//import { deletePost } from './data.js';
-//import { logout } from './data.js';
-//import { likePost } from './data.js';
+import { newPost, loadPosts, deletePost, likePost, logout } from './data.js';
+//  import { loadPosts } from './data.js';
+//  import { deletePost } from './data.js';
+//  import { logout } from './data.js';
+//  import { likePost } from './data.js';
 
 export const home = () => {
-  const container = document.createElement("div");
+  const container = document.createElement('div');
 
   container.innerHTML = ` 
     <nav>
@@ -22,24 +22,24 @@ export const home = () => {
     </section>
     `;
 
-  const resetForm = container.querySelector("#post-form");
-  const textPost = container.querySelector("#post-text");
-  const postButton = container.querySelector("#publish");
-  const editButton = container.querySelector("#edit-button");
-  const cancelEditBtn = container.querySelector("#cancel-edit");
-  const postPublic = container.querySelector("#public");
-  const postPrivate = container.querySelector("#privacy");
-  const addImage = container.querySelector("#image");
-  const timeline = container.querySelector("#timeline");
-  const buttonLogout = container.querySelector("#logout");
+  const resetForm = container.querySelector('#post-form');
+  const textPost = container.querySelector('#post-text');
+  const postButton = container.querySelector('#publish');
+  const editButton = container.querySelector('#edit-button');
+  const cancelEditBtn = container.querySelector('#cancel-edit');
+  const postPublic = container.querySelector('#public');
+  const postPrivate = container.querySelector('#privacy');
+  const addImage = container.querySelector('#image');
+  const timeline = container.querySelector('#timeline');
+  const buttonLogout = container.querySelector('#logout');
 
   const postTemplate = (array) => {
-    timeline.innerHTML = "";
+    timeline.innerHTML = '';
 
     // Template for the posts of the user
     array
       .map((post) => {
-        const template = document.createElement("div");
+        const template = document.createElement('div');
         template.innerHTML = `
         <div class='all-posts'>
           <div class='top'>Nome do usuário</div>
@@ -55,42 +55,42 @@ export const home = () => {
       `;
 
         // Deletes the post when clicked
-        const deletePostBtn = template.querySelector("#delete-post");
-        deletePostBtn.addEventListener("click", () => {
+        const deletePostBtn = template.querySelector('#delete-post');
+        deletePostBtn.addEventListener('click', () => {
           deletePost(deletePostBtn.dataset.postid);
         });
 
         /*************** KELLY VER!!!!!OK, RESOLVIDO*****************/
         // Likes the post when clicked
-        const likeButton = template.querySelector("#like-button");
-        likeButton.addEventListener("click", () => {
-          const likes = template.querySelector("#numbers-like"); //Aqui tive q fazer uma qs com o nome correto da id, estava like e deveria ser numbers-like
-          const numberLike = parseInt(likes.textContent) + 1; //Aqui coloquei o paserint para q transformasse o texto em numero e acrescentasse mais um conforme numero de curtida
+        const likeButton = template.querySelector('#like-button');
+        likeButton.addEventListener('click', () => {
+          const likes = template.querySelector('#numbers-like'); // Aqui tive q fazer uma qs com o nome correto da id, estava like e deveria ser numbers-like
+          const numberLike = parseInt(likes.textContent) + 1; //  Aqui coloquei o paserint para q transformasse o texto em numero e acrescentasse mais um conforme numero de curtida
 
-          likePost(likeButton.dataset.postid, numberLike); //Aqui só troquei o like por numberLike
-          likes.textContent = numberLike; //Aqui o texto de likes é igual o numero de like
+          likePost(likeButton.dataset.postid, numberLike); // Aqui só troquei o like por numberLike
+          likes.textContent = numberLike; //  Aqui o texto de likes é igual o numero de like
         });
 
         // Refresh timeline
         timeline.appendChild(template);
       })
-      .join("");
+      .join('');
   };
 
   timeline.innerHTML = loadPosts(postTemplate);
 
-  postButton.addEventListener("click", (event) => {
+  postButton.addEventListener('click', (event) => {
     event.preventDefault();
-    if (textPost.value === "") {
+    if (textPost.value === '') {
       return;
     }
     newPost(textPost.value);
-    timeline.innerHTML = "";
+    timeline.innerHTML = '';
     loadPosts(postTemplate);
     resetForm.reset();
   });
 
-  buttonLogout.addEventListener("click", logout);
+  buttonLogout.addEventListener('click', logout);
   return container;
 };
 
