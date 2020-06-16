df49bd1b8e995be37fd941e9605e5b57209a3b1c
larissamiyaji <natsumi-23@hotmail.com>
2020-06-15 23:02:14 -0300

    Função de login com o GitHub

--- public/pages/login/data.js
+++ public/pages/login/data.js
@@ -23,25 +23,29 @@ export const loginGoogle = () => {
       // const user = result.user;
     });
 };
-/*
+
 // Função para logar com a conta Github
+export const loginGithub = () => {
+  const provider = new firebase.auth.GithubAuthProvider();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(() => {
+      window.location.hash = 'home';
+      // const token = result.credential.accessToken;
+      // const user = result.user;
+    });
+};
 
-// export const provider = new firebase.auth.GithubAuthProvider();
-
-// firebase.auth().signInWithPopup(provider).then(function(result) {
-  //   // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-  //   const token = result.credential.accessToken;
-  //   // The signed-in user info.
-  //   const user = result.user;
-  //   // ...
-  // }).catch(function(error) {
-  //   // Handle Errors here.
-  //   const errorCode = error.code;
-  //   const errorMessage = error.message;
-  //   // The email of the user's account used.
-  //   const email = error.email;
-  // });
-*/
+// CATCH LOGIN COM GITHUB
+    /*
+    .catch(function (error) {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+    });*/
 
 /* CATCH LOGIN COM GOOGLE
 .catch(function (error) {
