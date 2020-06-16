289ff111d22d79e6e67ddebc57e17079aaa70451
Tamires Cordeiro <mirescordeiro@gmail.com>
2020-06-16 15:58:53 -0300

    login e media querie
    Co-authored-by: larissamiyaji <natsumi-23@hotmail.com>

--- public/pages/login/main.js
+++ public/pages/login/main.js
@@ -9,24 +9,25 @@ export const login = () => {
         <img src="./img/login_mobile.svg" alt="Ilustração de dois navegadores abertos, no primeiro há uma seta grande que clica no segundo, no segundo há uma ovelha com uma mensagem de emoticon de coração" class="mobile">
         <img src="./img/login_desktop.svg" alt="Ilustração de três navegadores abertos, no primeiro há um coração, no segundo uma seta de correto, no terceiro uma emoticon sorrindo. No centro entre eles temos uma ovelha" class="desktop">
       </figure>
-      <form class="flex center column login">
-        <h1>mentor<strong>she</strong></h1>
-        <p class="welcome mobile">&#60;seja bem-vinda!&#62;</p>
-        <p class="welcome desktop">&#60;mentoria para todas!&#62;</p>
-        <input id="user-email" type="email" placeholder="Email" required>
-        <span id="email-alert" class="alert"></span>
-        <input id="user-pass" type="password" placeholder="Senha" required>
-        <span id="pass-alert" class="alert"></span>
-        <button id="login-button" type="submit">ENTRAR</button>
-        <div class"google-github">
+      <div class="flex column">
+        <form class="flex center column login">
+          <h1>mentor<strong>she</strong></h1>
+          <p class="welcome mobile">&#60;seja bem-vinda!&#62;</p>
+          <p class="welcome desktop">&#60;mentoria para todas!&#62;</p>
+          <input id="user-email" type="email" placeholder="Email" required>
+          <span id="email-alert" class="alert"></span>
+          <input id="user-pass" type="password" placeholder="Senha" required>
+          <span id="pass-alert" class="alert"></span>
+          <button id="login-button" type="submit">ENTRAR</button>
+        </form>
+        <div class="google-github">
           <p>Ou entre com</p>
-          <button id="google-button" type="submit">GOOGLE</button>
-          <button id="github-button" type="submit">GITHUB</button>
-          <button id="facebook-button" type="submit">FACEBOOK</button>
+          <button id="google-button" type="submit"><span class="icon-google"></span></button>
+          <button id="github-button" type="submit"><span class="icon-github"></span></button>
         </div>
         <p class="footer">Não possui uma conta? <a href="#newAccount">Crie uma conta</a></p>
         <span id=validation-login></span>
-      </form>
+      </div>
     </section>
   `;
 
@@ -48,11 +49,6 @@ export const login = () => {
   githubButton.addEventListener('click', (event) => {
     event.preventDefault();
     loginGithub();
-  });
-
-  facebookButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    loginFacebook();
   });
 
   loginButton.addEventListener('click', (event) => {
@@ -92,3 +88,11 @@ export const login = () => {
   });
   return container;
 };
+
+/* 
+<button id="facebook-button" type="submit">FACEBOOK</button>
+facebookButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    loginFacebook();
+  });
+*/
